9b5ee7edbc0e40fb852d4294ca963d09cec471ad
Matt Stine <mstine@gopivotal.com>
2015-05-21 23:57:51 -0700

    fix username

--- labs/setup_pact.sh
+++ labs/setup_pact.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/vagrant/.rvm/scripts/rvm
+source /home/workshop/.rvm/scripts/rvm
 gem install bundler
 git clone https://github.com/bethesque/pact_broker.git
 cd pact_broker/example
 bundle install
-
